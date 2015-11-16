108626f985902c64357cffa015198f9861a5f3b5
Dirk Baeumer <dirkb@microsoft.com>
2015-11-16 22:14:54 +0100

    Hello vscode language server example

---
+++ server/src/server.ts
@@ -0,0 +1,94 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+'use strict';
+
+import {
+	createConnection, IConnection,
+	TextDocuments, ITextDocument, Diagnostic, DiagnosticSeverity,
+	InitializeParams, InitializeResult
+} from 'vscode-languageserver';
+
+// Create a connection for the server. The connection uses 
+// stdin / stdout for message passing
+let connection: IConnection = createConnection(process.stdin, process.stdout);
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
+let documents: TextDocuments = new TextDocuments();
+// Make the text document manager listen on the connection
+// for open, change and close text document events
+documents.listen(connection);
+
+// After the server has started the client sends an initilize request. The server receives
+// in the passed params the rootPath of the workspace plus the client capabilites. 
+let workspaceRoot: string;
+connection.onInitialize((params): InitializeResult => {
+	workspaceRoot = params.rootPath;
+	return {
+		capabilities: {
+			// Tell the client that the server works in FULL text document sync mode
+			textDocumentSync: documents.syncKind
+		}
+	}
+});
+
+// The content of a text document has changed. This event is emitted
+// when the text document first opened or when its content has changed.
+documents.onDidChangeContent((change) => {
+	validateTextDocument(change.document);
+});
+
+// The settings interface describe the server relevant settings part
+interface Settings {
+	languageServerExample: ExampleSettings;
+}
+
+// These are the example settings we defined in the client's package.json
+// file
+interface ExampleSettings {
+	maxNumberOfProblems: number;
+}
+
+// hold the maxNumberOfProblems setting
+let maxNumberOfProblems: number;
+// The settings have changed. Is send on server activation
+// as well.
+connection.onDidChangeConfiguration((change) => {
+	let settings = <Settings>change.settings;
+	maxNumberOfProblems = settings.languageServerExample.maxNumberOfProblems || 100;
+	// Revalidate any open text documents
+	documents.all().forEach(validateTextDocument);
+});
+
+function validateTextDocument(textDocument: ITextDocument): void {
+	let diagnostics: Diagnostic[] = [];
+	let lines = textDocument.getText().split(/\r?\n/g);
+	let problems = 0;
+	for (var i = 0; i < lines.length && problems < maxNumberOfProblems; i++) {
+		let line = lines[i];
+		let index = line.indexOf('typescript');
+		if (index >= 0) {
+			problems++;
+			diagnostics.push({
+				severity: DiagnosticSeverity.Warning,
+				range: {
+					start: { line: i, character: index},
+					end: { line: i, character: index + 10 }
+				},
+				message: `${line.substr(index, 10)} should be spelled TypeScript`
+			});
+		}
+	}
+	// Send the computed diagnostics to VSCode.
+	connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
+}
+
+connection.onDidChangeWatchedFiles((change) => {
+	// Monitored files have change in VSCode
+	connection.console.log('We recevied an file change event');
+});
+
+// Listen on the connection
+connection.listen();
