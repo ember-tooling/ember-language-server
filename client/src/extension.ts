b04601ec436ba520f0f74ffa42bec92735be6439
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-19 21:31:57 +0100

    client: Rename to "Ember Language Server"

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -37,7 +37,7 @@ export function activate(context: ExtensionContext) {
 	}
 	
 	// Create the language client and start the client.
-	let disposable = new LanguageClient('languageServerExample', 'Language Server Example', serverOptions, clientOptions).start();
+	let disposable = new LanguageClient('emberLanguageServer', 'Ember Language Server', serverOptions, clientOptions).start();
 	
 	// Push the disposable to the context's subscriptions so that the 
 	// client can be deactivated on extension deactivation
