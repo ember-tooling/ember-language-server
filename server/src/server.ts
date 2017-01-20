cff5ac9f25d2fc85c2acf5b7e0c7be8967e9e492
Tobias Bieniek <tobias.bieniek@gmail.com>
2017-01-20 01:01:18 +0100

    server: Add basic document symbols implementation for handlebars

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,13 +4,20 @@
  * ------------------------------------------------------------------------------------------ */
 'use strict';
 
-import { basename, dirname } from 'path';
+import { basename, dirname, extname } from 'path';
+import { readFileSync } from 'fs';
 
 import {
 	IPCMessageReader, IPCMessageWriter,
 	createConnection, IConnection,
-	TextDocuments, InitializeResult, InitializeParams,
+	TextDocuments, InitializeResult, InitializeParams, DocumentSymbolParams,
 } from 'vscode-languageserver';
+
+import { uriToFilePath } from 'vscode-languageserver/lib/files';
+
+import { SymbolInformation, SymbolKind, Range, Position } from 'vscode-languageserver-types';
+
+import { preprocess, traverse } from '@glimmer/syntax';
 
 const klaw = require('klaw');
 
@@ -41,6 +48,7 @@ export default class Server {
 		this.connection.onInitialize(this.onInitialize.bind(this));
 		this.documents.onDidChangeContent(this.onDidChangeContent.bind(this));
 		this.connection.onDidChangeWatchedFiles(this.onDidChangeWatchedFiles.bind(this));
+		this.connection.onDocumentSymbol(this.onDocumentSymbol.bind(this));
 	}
 
 	listen() {
@@ -62,6 +70,8 @@ export default class Server {
 			capabilities: {
 				// Tell the client that the server works in FULL text document sync mode
 				textDocumentSync: this.documents.syncKind,
+
+				documentSymbolProvider: true,
 			}
 		}
 	}
@@ -73,6 +83,37 @@ export default class Server {
 	private onDidChangeWatchedFiles(change) {
 		// here be dragons
 	}
+
+	private onDocumentSymbol(params: DocumentSymbolParams): SymbolInformation[] {
+		let uri = params.textDocument.uri;
+		let filePath = uriToFilePath(uri);
+		let extension = extname(filePath);
+		if (extension === '.hbs') {
+			let ast = preprocess(readFileSync(filePath, 'utf-8'));
+
+			let symbols: SymbolInformation[] = [];
+
+			traverse(ast, {
+				BlockStatement(node) {
+					if (node.program.blockParams.length === 0) return;
+
+					node.program.blockParams.forEach(blockParam => {
+						let symbol = SymbolInformation.create(blockParam, SymbolKind.Variable, locToRange(node.loc));
+						symbols.push(symbol);
+					});
+				}
+			});
+
+			return symbols;
+		}
+		return [];
+	}
+}
+
+function locToRange(loc): Range {
+	let start = Position.create(loc.start.line - 1, loc.start.column);
+	let end = Position.create(loc.end.line - 1, loc.end.column);
+	return Range.create(start, end);
 }
 
 export function findProjectRoots(workspaceRoot: string): Promise<string[]> {
