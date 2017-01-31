22cf2233463d191f0726fb463db1661d76ca36d9
t-sauer <t-sauer@outlook.de>
2017-01-31 22:57:04 +0100

    Changed EmberHelperConfig type to EmberCompletionItem class

--- server/src/completion-provider/ember-helpers.ts
+++ server/src/completion-provider/ember-helpers.ts
@@ -4,45 +4,52 @@ import {
 } from 'vscode-languageserver';
 
 type UsableIn = 'BlockPath' | 'MustachePath' | 'SubExpressionPath';
-type EmberHelperConfig = [string, CompletionItemKind, UsableIn[]];
 
 const HelperItem = CompletionItemKind.Function;
 const ComponentItem = CompletionItemKind.Class;
 
-const emberHelperConfigs: EmberHelperConfig[] = [
-  ['action',       HelperItem,    ['MustachePath', 'SubExpressionPath']],
-  ['component',    HelperItem,    ['BlockPath', 'MustachePath', 'SubExpressionPath']],
-  ['concat',       HelperItem,    ['MustachePath', 'SubExpressionPath']],
-  ['debugger',     HelperItem,    ['MustachePath']],
-  ['each',         HelperItem,    ['BlockPath']],
-  ['each-in',      HelperItem,    ['BlockPath']],
-  ['get',          HelperItem,    ['MustachePath', 'SubExpressionPath']],
-  ['hash',         HelperItem,    ['SubExpressionPath']],
-  ['if',           HelperItem,    ['BlockPath', 'MustachePath', 'SubExpressionPath']],
-  ['input',        ComponentItem, ['MustachePath']],
-  ['link-to',      ComponentItem, ['MustachePath']],
-  ['loc',          HelperItem,    ['MustachePath', 'SubExpressionPath']],
-  ['log',          HelperItem,    ['MustachePath']],
-  ['mount',        HelperItem,    ['MustachePath']],
-  ['mut',          HelperItem,    ['SubExpressionPath']],
-  ['outlet',       HelperItem,    ['MustachePath']],
-  ['partial',      HelperItem,    ['MustachePath']],
-  ['query-params', HelperItem,    ['SubExpressionPath']],
-  ['render',       HelperItem,    ['MustachePath']],
-  ['textarea',     ComponentItem, ['MustachePath']],
-  ['unbound',      HelperItem,    ['MustachePath', 'SubExpressionPath']],
-  ['unless',       HelperItem,    ['BlockPath', 'MustachePath', 'SubExpressionPath']],
-  ['with',         HelperItem,    ['BlockPath']]
+class EmberCompletionItem implements CompletionItem {
+
+  public detail: string;
+
+  constructor(
+    public label: string,
+    public kind: CompletionItemKind,
+    public usableIn: UsableIn[]
+  ) {
+  }
+}
+
+const emberHelperConfigs: EmberCompletionItem[] = [
+  new EmberCompletionItem('action',       HelperItem,    ['MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('component',    HelperItem,    ['BlockPath', 'MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('concat',       HelperItem,    ['MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('debugger',     HelperItem,    ['MustachePath']),
+  new EmberCompletionItem('each',         HelperItem,    ['BlockPath']),
+  new EmberCompletionItem('each-in',      HelperItem,    ['BlockPath']),
+  new EmberCompletionItem('get',          HelperItem,    ['MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('hash',         HelperItem,    ['SubExpressionPath']),
+  new EmberCompletionItem('if',           HelperItem,    ['BlockPath', 'MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('input',        ComponentItem, ['MustachePath']),
+  new EmberCompletionItem('link-to',      ComponentItem, ['MustachePath']),
+  new EmberCompletionItem('loc',          HelperItem,    ['MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('log',          HelperItem,    ['MustachePath']),
+  new EmberCompletionItem('mount',        HelperItem,    ['MustachePath']),
+  new EmberCompletionItem('mut',          HelperItem,    ['SubExpressionPath']),
+  new EmberCompletionItem('outlet',       HelperItem,    ['MustachePath']),
+  new EmberCompletionItem('partial',      HelperItem,    ['MustachePath']),
+  new EmberCompletionItem('query-params', HelperItem,    ['SubExpressionPath']),
+  new EmberCompletionItem('render',       HelperItem,    ['MustachePath']),
+  new EmberCompletionItem('textarea',     ComponentItem, ['MustachePath']),
+  new EmberCompletionItem('unbound',      HelperItem,    ['MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('unless',       HelperItem,    ['BlockPath', 'MustachePath', 'SubExpressionPath']),
+  new EmberCompletionItem('with',         HelperItem,    ['BlockPath'])
 ];
 
-function filterConfigs(type: UsableIn): EmberHelperConfig[] {
-  return emberHelperConfigs.filter(([, , types]) => types.includes(type));
+function filterConfigs(type: UsableIn): EmberCompletionItem[] {
+  return emberHelperConfigs.filter(({ usableIn }) => usableIn.includes(type));
 }
 
-function createCompletionItem([label, kind]: EmberHelperConfig): CompletionItem {
-  return { label, kind };
-}
-
-export const emberBlockItems: CompletionItem[] = filterConfigs('BlockPath').map(createCompletionItem);
-export const emberMustacheItems: CompletionItem[] = filterConfigs('MustachePath').map(createCompletionItem);
-export const emberSubExpressionItems: CompletionItem[] = filterConfigs('SubExpressionPath').map(createCompletionItem);
+export const emberBlockItems: CompletionItem[] = filterConfigs('BlockPath');
+export const emberMustacheItems: CompletionItem[] = filterConfigs('MustachePath');
+export const emberSubExpressionItems: CompletionItem[] = filterConfigs('SubExpressionPath');
