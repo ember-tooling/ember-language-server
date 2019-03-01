b43e962dc5e29758fe596fc3864ee42078cdcb4d
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-03-01 21:22:25 +0300

    go meta!

--- src/definition-providers/script.ts
+++ src/definition-providers/script.ts
@@ -13,9 +13,70 @@ import {
   podModulePrefixForRoot
 } from './../utils/layout-helpers';
 
-export default class ScriptDefinietionProvider {
-  constructor(private server: Server) {}
+type ItemType = 'Model' | 'Transform';
+type LayoutCollectorFn = (root: string, typeName: string, podModulePrefix?: string) => string[];
 
+function joinPaths(...args: string[]) {
+  return ['.ts', '.js'].map((extName: string) => {
+    const localArgs = args.slice(0);
+    const lastArg = localArgs.pop() + extName;
+    return path.join.apply(path, [...localArgs, lastArg]);
+  });
+}
+
+class PathResolvers {
+  [key: string]: LayoutCollectorFn;
+  muModelPaths(root: string, modelName: string) {
+    return joinPaths(root, 'src', 'data', 'models', modelName, 'model');
+  }
+  muTransformPaths(root: string, transformName: string) {
+    return joinPaths(root, 'src', 'data', 'transforms', transformName);
+  }
+  classicModelPaths(root: string, modelName: string) {
+    return joinPaths(root, 'app', 'models', modelName);
+  }
+  classicTransformPaths(root: string, transformName: string) {
+    return joinPaths(root, 'app', 'transforms', transformName);
+  }
+  podTransformPaths(root: string, transformName: string, podPrefix: string) {
+    return joinPaths(root, 'app', podPrefix, transformName, 'transform');
+  }
+  podModelPaths(root: string, modelName: string, podPrefix: string) {
+    return joinPaths(root, 'app', podPrefix, modelName, 'model');
+  }
+}
+
+export default class ScriptDefinietionProvider {
+  private resolvers!: PathResolvers;
+  constructor(private server: Server) {
+    this.resolvers = new PathResolvers();
+  }
+  guessPathsForType(root: string, fnName: ItemType, typeName: string) {
+    const guessedPaths: string[] = [];
+
+    if (isModuleUnificationApp(root)) {
+      this.resolvers[`mu${fnName}Paths`](root, typeName).forEach(
+        (pathLocation: string) => {
+          guessedPaths.push(pathLocation);
+        }
+      );
+    } else {
+      this.resolvers[`classic${fnName}Paths`](root, typeName).forEach(
+        (pathLocation: string) => {
+          guessedPaths.push(pathLocation);
+        }
+      );
+      const podPrefix = podModulePrefixForRoot(root);
+      if (podPrefix) {
+        this.resolvers[`pod${fnName}Paths`](root, typeName, podPrefix).forEach(
+          (pathLocation: string) => {
+            guessedPaths.push(pathLocation);
+          }
+        );
+      }
+    }
+    return pathsToLocations.apply(null, guessedPaths);
+  }
   handle(params: TextDocumentPositionParams, project: any): Definition | null {
     const uri = params.textDocument.uri;
     const { root } = project;
@@ -32,87 +93,12 @@ export default class ScriptDefinietionProvider {
     }
 
     if (isModelReference(astPath)) {
-      let modelName = astPath.node.value;
-
-      const guessedPaths: string[] = [];
-
-      if (isModuleUnificationApp(root)) {
-        this.muModelPaths(root, modelName).forEach(
-          (pathLocation: string) => {
-            guessedPaths.push(pathLocation);
-          }
-        );
-      } else {
-        this.classicModelPaths(root, modelName).forEach(
-          (pathLocation: string) => {
-            guessedPaths.push(pathLocation);
-          }
-        );
-        const podPrefix = podModulePrefixForRoot(root);
-        if (podPrefix) {
-          this.podModelPaths(root, modelName, podPrefix).forEach(
-            (pathLocation: string) => {
-              guessedPaths.push(pathLocation);
-            }
-          );
-        }
-      }
-      return pathsToLocations.apply(null, guessedPaths);
+      const modelName = astPath.node.value;
+      return this.guessPathsForType(root, 'Model', modelName);
     } else if (isTransformReference(astPath)) {
-      let transformName = astPath.node.value;
-      const guessedPaths: string[] = [];
-
-      if (isModuleUnificationApp(root)) {
-        this.muTransformPaths(root, transformName).forEach(
-          (pathLocation: string) => {
-            guessedPaths.push(pathLocation);
-          }
-        );
-      } else {
-        this.classicTransformPaths(root, transformName).forEach(
-          (pathLocation: string) => {
-            guessedPaths.push(pathLocation);
-          }
-        );
-
-        const podPrefix = podModulePrefixForRoot(root);
-        if (podPrefix) {
-          this.podTransformPaths(
-            root,
-            transformName,
-            podPrefix
-          ).forEach((pathLocation: string) => {
-            guessedPaths.push(pathLocation);
-          });
-        }
-      }
-      return pathsToLocations.apply(null, guessedPaths);
+      const transformName = astPath.node.value;
+      return this.guessPathsForType(root, 'Transform', transformName);
     }
     return null;
-  }
-  joinPaths(...args: string[]) {
-    return ['.ts', '.js'].map((extName: string) => {
-      const localArgs = args.slice(0);
-      const lastArg = localArgs.pop() + extName;
-      return path.join.apply(path, [...localArgs, lastArg]);
-    });
-  }
-  muModelPaths(root: string, modelName: string) {
-    return this.joinPaths(root, 'src', 'data', 'models', modelName, 'model');
-  }
-  muTransformPaths(root: string, transformName: string) {
-    return this.joinPaths(root, 'src', 'data', 'transforms', transformName);
-  }
-  classicModelPaths(root: string, modelName: string) {
-    return this.joinPaths(root, 'app', 'models', modelName);
-  }
-  classicTransformPaths(root: string, transformName: string) {
-    return this.joinPaths(root, 'app', 'transforms', transformName);
-  }
-  podTransformPaths(root: string, transformName: string, podPrefix: string) {
-    return this.joinPaths(root, 'app', podPrefix, transformName, 'transform');
-  }
-  podModelPaths(root: string, modelName: string, podPrefix: string) {
-    return this.joinPaths(root, 'app', podPrefix, modelName, 'model');
   }
 }
