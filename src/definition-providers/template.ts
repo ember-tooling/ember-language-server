0ce04aab26b6b0e5aea90acbd0b687a9a0ccb8e5
Chris Krycho <hello@chriskrycho.com>
2019-09-20 15:24:30 -0600

    style: format and implementation of template provider

--- src/definition-providers/template.ts
+++ src/definition-providers/template.ts
@@ -1,26 +1,21 @@
-import * as path from 'path';
-import * as fs from 'fs';
+import { join } from "path";
+import * as fs from "fs";
 
 import {
   RequestHandler,
   TextDocumentPositionParams,
   Definition
-} from 'vscode-languageserver';
+} from "vscode-languageserver";
 
-import {
-  isLinkToTarget
-} from './../utils/ast-helpers';
-
-import { toPosition } from './../estree-utils';
-import Server from './../server';
-import ASTPath from './../glimmer-utils';
-import { join } from 'path';
-
+import { isLinkToTarget } from "./../utils/ast-helpers";
+import { toPosition } from "./../estree-utils";
+import Server from "./../server";
+import ASTPath from "./../glimmer-utils";
 import {
   isTemplatePath,
   getComponentNameFromURI,
   getPodModulePrefix
-} from './../utils/layout-helpers';
+} from "./../utils/layout-helpers";
 
 import {
   getAbstractHelpersParts,
@@ -29,34 +24,43 @@ import {
   getPathsForComponentScripts,
   pathsToLocationsWithPosition,
   pathsToLocations
-} from './../utils/definition-helpers';
+} from "./../utils/definition-helpers";
 
-import { kebabCase }  from 'lodash';
-import { preprocess } from '@glimmer/syntax';
-import { Project } from '../project-roots';
+import { kebabCase } from "lodash";
+import { preprocess } from "@glimmer/syntax";
+import { Project } from "../project-roots";
 
 function normalizeAngleTagName(tagName: string) {
-  return tagName.split('::').map((item: string) => kebabCase(item)).join('/');
+  return tagName
+    .split("::")
+    .map((item: string) => kebabCase(item))
+    .join("/");
 }
 
 function looksLikeClassicComponentName(name: string) {
-  return name.length && !name.includes('.') && !name.includes(' ') && name === name.toLowerCase();
+  return (
+    name.length &&
+    !name.includes(".") &&
+    !name.includes(" ") &&
+    name === name.toLowerCase()
+  );
 }
 
 function extractValueForMaybeClassicComponentName(focusPath: ASTPath) {
-  let value = '';
-  const node = focusPath.node;
-  const parent = focusPath.parent;
+  let value = "";
+  const { node, parent } = focusPath;
   if (!parent) {
     return value;
   }
-  if (node.type === 'StringLiteral' && parent.type === 'HashPair') {
+
+  if (node.type === "StringLiteral" && parent.type === "HashPair") {
     value = node.original;
-  } else if (node.type === 'TextNode' && parent.type === 'AttrNode') {
+  } else if (node.type === "TextNode" && parent.type === "AttrNode") {
     value = node.chars;
   }
+
   return value;
-};
+}
 
 function maybeClassicComponentName(focusPath: ASTPath) {
   let value = extractValueForMaybeClassicComponentName(focusPath);
@@ -64,7 +68,7 @@ function maybeClassicComponentName(focusPath: ASTPath) {
 }
 
 export function provideRouteDefinition(root: string, routeName: string) {
-  const routeParts = routeName.split('.');
+  const routeParts = routeName.split(".");
   const lastRoutePart = routeParts.pop();
   if (!lastRoutePart) {
     throw new Error(`Route name ${routeName} is ill-formed!`);
@@ -80,81 +84,90 @@ export function provideRouteDefinition(root: string, routeName: string) {
 
   const podPrefix = getPodModulePrefix(root);
   if (podPrefix) {
-    routePaths.push(join(root, 'app', podPrefix, ...routeParts, lastRoutePart, 'route.js'));
-    routePaths.push(join(root, 'app', podPrefix, ...routeParts, lastRoutePart, 'route.ts'));
-    routePaths.push(join(root, 'app', podPrefix, ...routeParts, lastRoutePart, 'controller.js'));
-    routePaths.push(join(root, 'app', podPrefix, ...routeParts, lastRoutePart, 'controller.ts'));
-    routePaths.push(join(root, 'app', podPrefix, ...routeParts, lastRoutePart, 'template.hbs'));
+    for (const file in [
+      "route.js",
+      "route.ts",
+      "controller.js",
+      "controller.ts",
+      "template.hbs"
+    ]) {
+      routePaths.push(
+        join(root, "app", podPrefix, ...routeParts, lastRoutePart, file)
+      );
+    }
   }
 
   return pathsToLocations(...routePaths);
 }
 
-function provideAngleBrackedComponentDefinition(root: string, focusPath: ASTPath) {
+function provideAngleBrackedComponentDefinition(
+  root: string,
+  focusPath: ASTPath
+) {
   const maybeComponentName = normalizeAngleTagName(focusPath.node.tag);
 
-  let paths = [
+  const appPaths = [
     ...getPathsForComponentScripts(root, maybeComponentName),
     ...getPathsForComponentTemplates(root, maybeComponentName)
   ].filter(fs.existsSync);
 
-  if (!paths.length) {
-    paths = getAddonPathsForComponentTemplates(root, maybeComponentName);
-  }
+  const paths =
+    appPaths.length > 0
+      ? appPaths
+      : getAddonPathsForComponentTemplates(root, maybeComponentName);
 
-  return pathsToLocations.apply(
-    null,
-    paths.length > 1
-      ? paths.filter((postfix: string) => isTemplatePath(postfix))
-      : paths
-  );
+  return pathsToLocations(...paths.filter(isTemplatePath));
 }
 
 function provideBlockComponentDefinition(root: string, focusPath: ASTPath) {
   let maybeComponentName = focusPath.node.path.original;
-  let paths: string[] = getPathsForComponentTemplates(
+  const appPaths = getPathsForComponentTemplates(
     root,
     maybeComponentName
   ).filter(fs.existsSync);
-  if (!paths.length) {
-    paths = getAddonPathsForComponentTemplates(root, maybeComponentName).filter(
-      (name: string) => {
-        return isTemplatePath(name);
-      }
-    );
-  }
+
+  const paths =
+    appPaths.length > 0
+      ? appPaths
+      : getAddonPathsForComponentTemplates(root, maybeComponentName).filter(
+          isTemplatePath
+        );
+
   // getAddonPathsForComponentTemplates
-  return pathsToLocationsWithPosition(paths, '{{yield');
+  return pathsToLocationsWithPosition(paths, "{{yield");
 }
 
-function providePropertyDefinition(root: string, focusPath: ASTPath, uri: string) {
+function providePropertyDefinition(
+  root: string,
+  focusPath: ASTPath,
+  uri: string
+) {
   let maybeComponentName = getComponentNameFromURI(root, uri);
   if (!maybeComponentName) {
     return null;
   }
-  let paths: string[] = getPathsForComponentScripts(
-    root,
-    maybeComponentName
-  ).filter(fs.existsSync);
-  if (!paths.length) {
-    paths = getAddonPathsForComponentTemplates(root, maybeComponentName).filter(
-      (name: string) => {
-        return !isTemplatePath(name);
-      }
-    );
-  }
+
+  const appPaths = getPathsForComponentScripts(root, maybeComponentName).filter(
+    fs.existsSync
+  );
+
+  let paths =
+    appPaths.length > 0
+      ? appPaths
+      : getAddonPathsForComponentTemplates(root, maybeComponentName).filter(
+          name => !isTemplatePath(name)
+        );
+
   const text = focusPath.node.original;
   return pathsToLocationsWithPosition(
     paths,
-    text.replace('this.', '').split('.')[0]
+    text.replace("this.", "").split(".")[0]
   );
 }
 
 function provideComponentDefinition(root: string, maybeComponentName: string) {
-  let helpers = getAbstractHelpersParts(root, 'app', maybeComponentName).map(
-    (pathParts) => {
-      return path.join.apply(path, pathParts.filter((part: any) => !!part));
-    }
+  let helpers = getAbstractHelpersParts(root, "app", maybeComponentName).map(
+    pathParts => join(...pathParts.filter((part: any) => !!part))
   );
 
   let paths = [
@@ -175,7 +188,7 @@ function provideComponentDefinition(root: string, maybeComponentName: string) {
 
 function provideMustacheDefinition(root: string, focusPath: ASTPath) {
   const maybeComponentName =
-    focusPath.node.type === 'ElementNode'
+    focusPath.node.type === "ElementNode"
       ? normalizeAngleTagName(focusPath.node.tag)
       : focusPath.node.original;
   return provideComponentDefinition(root, maybeComponentName);
@@ -185,71 +198,52 @@ function provideHashPropertyUsage(root: string, focusPath: ASTPath) {
   let parentPath = focusPath.parentPath;
   if (parentPath && parentPath.parent && parentPath.parent.path) {
     const maybeComponentName = parentPath.parent.path.original;
-    if (
-      !maybeComponentName.includes('.') &&
-      maybeComponentName.includes('-')
-    ) {
-      let paths = [
+    if (!maybeComponentName.includes(".") && maybeComponentName.includes("-")) {
+      const appPaths = [
         ...getPathsForComponentScripts(root, maybeComponentName),
         ...getPathsForComponentTemplates(root, maybeComponentName)
       ].filter(fs.existsSync);
 
-      if (!paths.length) {
-        paths = getAddonPathsForComponentTemplates(root, maybeComponentName);
-      }
+      const paths = (appPaths.length
+        ? appPaths
+        : getAddonPathsForComponentTemplates(root, maybeComponentName)
+      ).filter(isTemplatePath);
 
-      const finalPaths =
-        paths.length > 1
-          ? paths.filter((postfix: string) => isTemplatePath(postfix))
-          : paths;
-      return pathsToLocationsWithPosition(
-        finalPaths,
-        '@' + focusPath.node.key
-      );
+      return pathsToLocationsWithPosition(paths, "@" + focusPath.node.key);
     }
   }
   return null;
 }
 
-function provideAngleBracketComponentAttributeUsage(root: string, focusPath: ASTPath) {
+function provideAngleBracketComponentAttributeUsage(
+  root: string,
+  focusPath: ASTPath
+) {
   const maybeComponentName = normalizeAngleTagName(focusPath.parent.tag);
 
-  let paths = [
+  const appPaths = [
     ...getPathsForComponentScripts(root, maybeComponentName),
     ...getPathsForComponentTemplates(root, maybeComponentName)
   ].filter(fs.existsSync);
 
-  if (!paths.length) {
-    paths = getAddonPathsForComponentTemplates(root, maybeComponentName);
-  }
+  const paths = (appPaths.length
+    ? appPaths
+    : getAddonPathsForComponentTemplates(root, maybeComponentName)
+  ).filter(isTemplatePath);
 
-  const finalPaths =
-    paths.length > 1
-      ? paths.filter((postfix: string) => isTemplatePath(postfix))
-      : paths;
-  return pathsToLocationsWithPosition(finalPaths, focusPath.node.name);
+  return pathsToLocationsWithPosition(paths, focusPath.node.name);
 }
 
 function isLocalProperty(path: ASTPath) {
-  let node = path.node;
-  if (node.type === 'PathExpression') {
-    return node.this;
-  }
-  return false;
+  return path.node.type === "PathExpression" && !!path.node.this;
 }
 
 function isHashPairKey(path: ASTPath) {
-  let node = path.node;
-  return node.type === 'HashPair';
+  return path.node.type === "HashPair";
 }
 
 function isAnglePropertyAttribute(path: ASTPath) {
-  let node = path.node;
-  if (node.type === 'AttrNode') {
-    if (node.name.charAt(0) === '@') {
-      return true;
-    }
-  }
+  return path.node.type === "AttrNode" && path.node.name.charAt(0) === "@";
 }
 
 function isActionName(path: ASTPath) {
@@ -258,58 +252,56 @@ function isActionName(path: ASTPath) {
     return false;
   }
   if (
-    path.parent.type !== 'MustacheStatement' &&
-    path.parent.type !== 'PathExpression' &&
-    path.parent.type !== 'SubExpression' &&
-    path.parent.type !== 'ElementModifierStatement'
+    path.parent.type !== "MustacheStatement" &&
+    path.parent.type !== "PathExpression" &&
+    path.parent.type !== "SubExpression" &&
+    path.parent.type !== "ElementModifierStatement"
   ) {
     return false;
   }
+
   if (
     !path.parent ||
-    path.parent.path.original !== 'action' ||
+    path.parent.path.original !== "action" ||
     !path.parent.params[0] === node
   ) {
     return false;
   }
-  if (node.type === 'StringLiteral') {
+
+  if (node.type === "StringLiteral") {
     return true;
-  } else if (node.type === 'PathExpression' && node.this) {
+  } else if (node.type === "PathExpression" && node.this) {
     return true;
   }
+
   return false;
 }
 
 function isComponentWithBlock(path: ASTPath) {
   let node = path.node;
   return (
-    node.type === 'BlockStatement' &&
-    node.path.type === 'PathExpression' &&
+    node.type === "BlockStatement" &&
+    node.path.type === "PathExpression" &&
     node.path.this === false &&
-    node.path.original.includes('-') &&
-    node.path.original.charAt(0) !== '-' &&
-    !node.path.original.includes('.')
+    node.path.original.includes("-") &&
+    node.path.original.charAt(0) !== "-" &&
+    !node.path.original.includes(".")
   );
 }
 
 function isAngleComponent(path: ASTPath) {
-  let node = path.node;
-
-  if (node.type === 'ElementNode') {
-    if (node.tag.charAt(0) === node.tag.charAt(0).toUpperCase()) {
-      return true;
-    }
-  }
+  return (
+    path.node.type === "ElementNode" &&
+    path.node.tag.charAt(0) === path.node.tag.charAt(0).toUpperCase()
+  );
 }
 
 function isComponentOrHelperName(path: ASTPath) {
-  let node = path.node;
-
   if (this.isAngleComponent(path)) {
     return true;
   }
 
-  if (node.type === 'StringLiteral') {
+  if (path.node.type === "StringLiteral") {
     // if (node.original.includes('/')) {
     //   return true;
     // } else if (!node.original.includes('.') && node.original.includes('-')) {
@@ -317,24 +309,24 @@ function isComponentOrHelperName(path: ASTPath) {
     // }
     if (
       path.parent &&
-      path.parent.path.original === 'component' &&
-      path.parent.params[0] === node
+      path.parent.path.original === "component" &&
+      path.parent.params[0] === path.node
     ) {
       return true;
     }
   }
 
-  if (node.type !== 'PathExpression') {
+  if (path.node.type !== "PathExpression") {
     return false;
   }
 
   let parent = path.parent;
   if (
     !parent ||
-    parent.path !== node ||
-    (parent.type !== 'MustacheStatement' &&
-      parent.type !== 'BlockStatement' &&
-      parent.type !== 'SubExpression')
+    parent.path !== path.node ||
+    (parent.type !== "MustacheStatement" &&
+      parent.type !== "BlockStatement" &&
+      parent.type !== "SubExpression")
   ) {
     return false;
   }
@@ -349,13 +341,17 @@ export default class TemplateDefinitionProvider {
     this.server = server;
   }
 
-  handle(params: TextDocumentPositionParams, project: Project): Definition | null {
+  handle(
+    params: TextDocumentPositionParams,
+    project: Project
+  ): Definition | null {
     let uri = params.textDocument.uri;
     const root = project.root;
     const document = this.server.documents.get(uri);
     if (!document) {
       return null;
     }
+
     let content = document.getText();
     let ast = preprocess(content);
     let focusPath = ASTPath.toPosition(ast, toPosition(params.position));
@@ -364,8 +360,11 @@ export default class TemplateDefinitionProvider {
       return null;
     }
     // <FooBar @some-component-name="my-component" /> || {{some-component some-name="my-component/name"}}
-    if  (maybeClassicComponentName(focusPath)) {
-      return provideComponentDefinition(project.root, extractValueForMaybeClassicComponentName(focusPath));
+    if (maybeClassicComponentName(focusPath)) {
+      return provideComponentDefinition(
+        project.root,
+        extractValueForMaybeClassicComponentName(focusPath)
+      );
     } else if (isAngleComponent(focusPath)) {
       // <FooBar />
       return provideAngleBrackedComponentDefinition(root, focusPath);
@@ -374,10 +373,7 @@ export default class TemplateDefinitionProvider {
       return provideBlockComponentDefinition(root, focusPath);
 
       // {{action "fooBar"}}, (action "fooBar"), (action this.fooBar), this.someProperty
-    } else if (
-      isActionName(focusPath) ||
-      isLocalProperty(focusPath)
-    ) {
+    } else if (isActionName(focusPath) || isLocalProperty(focusPath)) {
       return providePropertyDefinition(root, focusPath, uri);
 
       // {{foo-bar}}
