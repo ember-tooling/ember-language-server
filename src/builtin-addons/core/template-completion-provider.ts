7e60a209ce210ff3a88e71d5ed3f79c293ac6255
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-11-21 21:19:29 +0300

    fix ci

--- src/builtin-addons/core/template-completion-provider.ts
+++ src/builtin-addons/core/template-completion-provider.ts
@@ -4,7 +4,7 @@ import { uniqBy, startCase, camelCase } from 'lodash';
 
 import * as memoize from 'memoizee';
 import * as fs from 'fs';
-import { emberBlockItems, emberMustacheItems, emberSubExpressionItems } from './ember-helpers';
+import { emberBlockItems, emberMustacheItems, emberSubExpressionItems, emberModifierItems } from './ember-helpers';
 import { templateContextLookup } from './template-context-provider';
 import { provideComponentTemplatePaths } from './template-definition-provider';
 
@@ -222,7 +222,7 @@ export default class TemplateCompletionProvider {
         const addonModifiers = mGetProjectAddonsInfo(root).filter(({ detail }: { detail: string }) => {
           return detail === 'modifier';
         });
-        completions.push(...uniqBy([...mListModifiers(root), ...addonModifiers, ...builtinModifiers()], 'label'));
+        completions.push(...uniqBy([...emberModifierItems, ...mListModifiers(root), ...addonModifiers, ...builtinModifiers()], 'label'));
       }
     } catch (e) {
       log('error', e);
