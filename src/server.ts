606fd33e586391045c8dbad59b107fb9852b0b74
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-12-23 22:50:45 +0300

    URI to file

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@
 
 import * as path from 'path';
 import * as fs from 'fs';
+import { URI } from 'vscode-uri';
 
 import {
   IPCMessageReader,
@@ -103,7 +104,7 @@ export default class Server {
       if (project) {
         const item = project.matchPathToType(fullPath);
         if (item) {
-          return this.getRegistry(project.root)[item.name] || [];
+          return (this.getRegistry(project.root)[item.name] || []).map((fPath: string) => URI.file(fPath));
         }
       }
 
