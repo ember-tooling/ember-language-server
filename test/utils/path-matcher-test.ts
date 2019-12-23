3fa243fd8b162506aeb4bfc124bc23dc4a6be0db
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-12-23 18:50:53 +0300

    empty case and custom prefix

--- test/utils/path-matcher-test.ts
+++ test/utils/path-matcher-test.ts
@@ -51,12 +51,80 @@ describe('PodMatcher', () => {
     expect(m('foo/bar/app/pods/foo-bar/service.js')).toEqual({ type: 'service', name: 'foo-bar' });
     expect(m('foo/bar/app/pods/foo-bar/baz/service.js')).toEqual({ type: 'service', name: 'foo-bar/baz' });
   });
+  it('empty', () => {
+    expect(m('foo/bar/app/components/foo/index.hbs')).toEqual(null);
+    expect(m('foo/bar/app/routes/foo.ts')).toEqual(null);
+    expect(m('foo/bar/app/helpers/foo.js')).toEqual(null);
+  });
+});
+
+describe('PodMatcher :customPrefix', () => {
+  const matcher = new PodMatcher('app');
+  function m(str: string) {
+    return matcher.metaFromPath(str);
+  }
+  it('components', () => {
+    expect(m('foo/bar/app/foo/component.ts')).toEqual({ type: 'component', name: 'foo' });
+    expect(m('foo/bar/app/foo/component.js')).toEqual({ type: 'component', name: 'foo' });
+    expect(m('foo/bar/app/foo/component.hbs')).toEqual({ type: 'component', name: 'foo' });
+    expect(m('foo/bar/app/foo/component.hbs')).toEqual({ type: 'component', name: 'foo' });
+  });
+  it('routes', () => {
+    expect(m('foo/bar/app/foo/route.ts')).toEqual({ type: 'route', name: 'foo' });
+    expect(m('foo/bar/app/foo/index/route.ts')).toEqual({ type: 'route', name: 'foo/index' });
+  });
+  it('controllers', () => {
+    expect(m('foo/bar/app/foo/controller.ts')).toEqual({ type: 'controller', name: 'foo' });
+    expect(m('foo/bar/app/foo/index/controller.ts')).toEqual({ type: 'controller', name: 'foo/index' });
+  });
+  it('templates', () => {
+    expect(m('foo/bar/app/foo/index/template.hbs')).toEqual({ type: 'template', name: 'foo/index' });
+    expect(m('foo/bar/app/foo/template.hbs')).toEqual({ type: 'template', name: 'foo' });
+  });
+  it('helpers', () => {
+    expect(m('foo/bar/app/foo/helper.js')).toEqual({ type: 'helper', name: 'foo' });
+    expect(m('foo/bar/app/foo-bar/helper.js')).toEqual({ type: 'helper', name: 'foo-bar' });
+  });
+  it('modifiers', () => {
+    expect(m('foo/bar/app/foo/modifier.js')).toEqual({ type: 'modifier', name: 'foo' });
+    expect(m('foo/bar/app/foo-bar/modifier.js')).toEqual({ type: 'modifier', name: 'foo-bar' });
+  });
+  it('models', () => {
+    expect(m('foo/bar/app/foo/model.js')).toEqual({ type: 'model', name: 'foo' });
+    expect(m('foo/bar/app/foo-bar/model.js')).toEqual({ type: 'model', name: 'foo-bar' });
+    expect(m('foo/bar/app/foo-bar/baz/model.js')).toEqual({ type: 'model', name: 'foo-bar/baz' });
+  });
+  it('serializers', () => {
+    expect(m('foo/bar/app/foo/serializer.js')).toEqual({ type: 'serializer', name: 'foo' });
+    expect(m('foo/bar/app/foo-bar/serializer.js')).toEqual({ type: 'serializer', name: 'foo-bar' });
+    expect(m('foo/bar/app/foo-bar/baz/serializer.js')).toEqual({ type: 'serializer', name: 'foo-bar/baz' });
+  });
+  it('transforms', () => {
+    expect(m('foo/bar/app/foo/transform.js')).toEqual({ type: 'transform', name: 'foo' });
+    expect(m('foo/bar/app/foo-bar/transform.js')).toEqual({ type: 'transform', name: 'foo-bar' });
+    expect(m('foo/bar/app/foo-bar/baz/transform.js')).toEqual({ type: 'transform', name: 'foo-bar/baz' });
+  });
+  it('services', () => {
+    expect(m('foo/bar/app/foo/service.js')).toEqual({ type: 'service', name: 'foo' });
+    expect(m('foo/bar/app/foo-bar/service.js')).toEqual({ type: 'service', name: 'foo-bar' });
+    expect(m('foo/bar/app/foo-bar/baz/service.js')).toEqual({ type: 'service', name: 'foo-bar/baz' });
+  });
+  it('empty', () => {
+    expect(m('foo/bar/app/components/foo/index.hbs')).toEqual(null);
+    expect(m('foo/bar/app/routes/foo.ts')).toEqual(null);
+    expect(m('foo/bar/app/helpers/foo.js')).toEqual(null);
+  });
 });
 describe('ClassicPathMatcher', () => {
   const matcher = new ClassicPathMatcher();
   function m(str: string) {
     return matcher.metaFromPath(str);
   }
+  it('empty', () => {
+    expect(m('foo/bar/app/pods/foo/modifier.js')).toEqual(null);
+    expect(m('foo/bar/app/pods/foo/transform.js')).toEqual(null);
+    expect(m('foo/bar/app/pods/foo/service.js')).toEqual(null);
+  });
   it('components', () => {
     expect(m('foo/bar/app/components/foo/index.ts')).toEqual({ type: 'component', name: 'foo' });
     expect(m('foo/bar/app/components/foo/index.js')).toEqual({ type: 'component', name: 'foo' });
