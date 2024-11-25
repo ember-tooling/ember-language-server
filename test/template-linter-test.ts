e2804b164223fe5bdb60b48cf9417bd01cf985aa
Alex Kanunnikov <lifeart92@gmail.com>
2024-11-25 21:47:42 +0300

    add failing test

---
+++ test/template-linter-test.ts
@@ -0,0 +1,37 @@
+import * as semver from 'semver';
+
+import TemplateLinter from '../src/template-linter';
+import { type Server } from '../src';
+import { TextDocument } from 'vscode-languageserver-textdocument';
+
+describe('template-linter', function () {
+  describe('sourcesForDocument', function () {
+    const linter = new TemplateLinter({
+      options: {
+        type: 'node',
+      },
+    } as Server);
+
+    it('supports empty template-lint version', function () {
+      const doc: TextDocument = {
+        uri: 'test.gjs',
+        getText() {
+          return 'let a = 12;<template>1</template>';
+        },
+      } as TextDocument;
+
+      expect(linter['sourcesForDocument'](doc, null)).toEqual(['let a = 12;<template>1</template>']);
+    });
+    it('process gjs for template-lint v4', function () {
+      const doc: TextDocument = {
+        uri: 'test.gjs',
+        getText() {
+          return 'let a = 12;<template>1</template>';
+        },
+      } as TextDocument;
+
+      console.log(semver.parse('~4.3.1'));
+      expect(linter['sourcesForDocument'](doc, semver.parse('~4.3.1'))).toEqual(['<template>1</template>']);
+    });
+  });
+});
